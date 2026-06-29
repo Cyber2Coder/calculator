08bc769f42bc3bd9bdeb426f9a318975827ac75f
Paul <mrpaulepatterson@gmail.com>
2026-06-28 17:33:47 -1000

    Git Repository Commit Message - Set

---
+++ script.js
@@ -0,0 +1,39 @@
+let firstNumber = "";
+let secondNumber = "";
+let currentOperator = null;
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      return null;
+  }
+}
+
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) return "Nope.";
+  return a / b;
+}
